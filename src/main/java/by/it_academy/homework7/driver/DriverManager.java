00d42d6069ca1f1af03b989bb6902a0f4f1402aa
raman-voitka <ramanvoitka@gmail.com>
2022-03-18 00:07:37 +0300

    changed DriverManager file

--- src/main/java/by/it_academy/homework7/driver/DriverManager.java
+++ src/main/java/by/it_academy/homework7/driver/DriverManager.java
@@ -1,17 +1,16 @@
 package by.it_academy.homework7.driver;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.Optional;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.openqa.selenium.chrome.ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY;
+import static org.openqa.selenium.firefox.GeckoDriverService.GECKO_DRIVER_EXE_PROPERTY;
 
 public class DriverManager {
 
-    private static final String FIREFOX_DRIVE_PATH = "C:\\Users\\raman\\GitProjects\\homework7\\src\\main\\resources\\drivers\\geckodriver_99.exe";
+    private static final String FIREFOX_DRIVE_PATH = "C:\\Users\\raman\\GitProjects\\homework7\\src\\main\\resources\\drivers\\geckodriver.exe";
     private static final int IMPLICIT_WAIT_TIMEOUT = 5;
     private static final int PAGE_LOAD_TIMEOUT = 40;
     private static ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
@@ -20,7 +19,7 @@ public class DriverManager {
     }
 
     public static void setupDriver() {
-        System.setProperty(CHROME_DRIVER_EXE_PROPERTY, FIREFOX_DRIVE_PATH);
+        System.setProperty(GECKO_DRIVER_EXE_PROPERTY, FIREFOX_DRIVE_PATH);
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT_TIMEOUT, SECONDS);
